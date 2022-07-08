78c33e15677b96eab828e48d2cbe1c395403bb35
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-07-08 12:52:58 -0300

    Complete AccuWeather API querys

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,138 +7,44 @@ import {
   View,
   ScrollView,
   TouchableOpacity,
+  FlatList,
 } from 'react-native'
+// import { ACCUWEATHERAPIKEY } from '@env'
 
 import clouds from '../../assets/imgs/clouds.png'
 import DailyForecast from '../components/DailyForecast'
-import HourlyWeather from '../components/HourlyForecast'
+import HourlyForecast from '../components/HourlyForecast'
 import axios from 'axios'
-
-const initialState = {
-  cityName: 'Blumenau',
-  current: {
-    date: '2022-07-07T14:20:00-03:00',
-    temp: 29,
-    icon: 7,
-    iconPhrase: 'Nublado',
-    hasPrecipitation: false,
-    precipitaionType: null,
-    isDayTime: true,
-  },
-  todayForecast: {
-    date: '2022-07-07T07:00:00-03:00',
-    minTemp: 9,
-    maxTemp: 30,
-    day: {
-      icon: 7,
-      iconPhrase: 'Nublado',
-      hasPrecipitation: false,
-      precipitaionType: null,
-      precipitaionIntensity: null,
-    },
-    night: {
-      icon: 34,
-      iconPhrase: 'Predominantemente aberto',
-      hasPrecipitation: false,
-      precipitaionType: null,
-      precipitaionIntensity: null,
-    },
-  },
-  day2Forecast: {
-    date: '2022-07-08T07:00:00-03:00',
-    minTemp: 14,
-    maxTemp: 26,
-    day: {
-      icon: 4,
-      iconPhrase: 'Nuvens esparsas',
-      hasPrecipitation: false,
-      precipitaionType: null,
-      precipitaionIntensity: null,
-    },
-    night: {
-      icon: 12,
-      iconPhrase: 'Pancadas de chuva',
-      hasPrecipitation: true,
-      precipitaionType: 'Rain',
-      precipitaionIntensity: 'Light',
-    },
-  },
-  day3Forecast: {
-    date: '2022-07-09T07:00:00-03:00',
-    minTemp: 14,
-    maxTemp: 26,
-    day: {
-      icon: 4,
-      iconPhrase: 'Nuvens esparsas',
-      hasPrecipitation: false,
-      precipitaionType: null,
-      precipitaionIntensity: null,
-    },
-    night: {
-      icon: 12,
-      iconPhrase: 'Pancadas de chuva',
-      hasPrecipitation: true,
-      precipitaionType: 'Rain',
-      precipitaionIntensity: 'Light',
-    },
-  },
-  day4Forecast: {
-    date: '2022-07-10T07:00:00-03:00',
-    minTemp: 14,
-    maxTemp: 26,
-    day: {
-      icon: 4,
-      iconPhrase: 'Nuvens esparsas',
-      hasPrecipitation: false,
-      precipitaionType: null,
-      precipitaionIntensity: null,
-    },
-    night: {
-      icon: 12,
-      iconPhrase: 'Pancadas de chuva',
-      hasPrecipitation: true,
-      precipitaionType: 'Rain',
-      precipitaionIntensity: 'Light',
-    },
-  },
-  day5Forecast: {
-    date: '2022-07-11T07:00:00-03:00',
-    minTemp: 14,
-    maxTemp: 26,
-    day: {
-      icon: 4,
-      iconPhrase: 'Nuvens esparsas',
-      hasPrecipitation: false,
-      precipitaionType: null,
-      precipitaionIntensity: null,
-    },
-    night: {
-      icon: 12,
-      iconPhrase: 'Pancadas de chuva',
-      hasPrecipitation: true,
-      precipitaionType: 'Rain',
-      precipitaionIntensity: 'Light',
-    },
-  },
-}
+import { initialState } from '../common'
 
 export default class Home extends Component {
   state = {
     ...initialState,
   }
 
+  getCityInfo = async () => {
+    try {
+      const cityName = this.state.cityName
+      const res = await axios.get(
+        `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=m7YVXQ5NAYdGRbXEygi5hyuFUcT9657F&q=${cityName}&language=pt-br&details=true&offset=1`
+      )
+      const cityCode = res.data[0].Key
+      this.setState({ cityCode })
+    } catch (e) {
+      console.log(e)
+    }
+  }
   getCurrentWeather = async () => {
     try {
-      const cityCode = '35954'
+      const cityCode = this.state.cityCode
       const res = await axios.get(
         `http://dataservice.accuweather.com/currentconditions/v1/${cityCode}?apikey=m7YVXQ5NAYdGRbXEygi5hyuFUcT9657F&language=pt-br`
       )
       const today = res.data[0]
-      // console.log(today)
       this.setState({
         current: {
           date: today.LocalObservationDateTime,
-          temp: today.Temperature.Metric.Value,
+          temp: Math.round(today.Temperature.Metric.Value),
           icon: today.WeatherIcon,
           iconPhrase: today.WeatherText,
           hasPrecipitation: today.HasPrecipitation,
@@ -152,44 +58,93 @@ export default class Home extends Component {
   }
   get5DayForecast = async () => {
     try {
-      const cityCode = '35954'
+      const cityCode = this.state.cityCode
       const res = await axios.get(
         `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityCode}?apikey=m7YVXQ5NAYdGRbXEygi5hyuFUcT9657F&language=pt-br&metric=true`
       )
-      const today = res.data.DailyForecasts[0]
-      console.log(today)
+      const dailyForecast = []
+      for (day of res.data.DailyForecasts) {
+        dailyForecast.push(this.dailyForecast(day))
+      }
+
       this.setState({
-        todayForecast: {
-          date: today.Date,
-          minTemp: today.Temperature.Minimum.Value,
-          maxTemp: today.Temperature.Maximum.Value,
-          day: {
-            icon: today.Day.Icon,
-            iconPhrase: today.Day.IconPhrase,
-            hasPrecipitation: today.Day.HasPrecipitation,
-            precipitaionType: today.Day.HasPrecipitation
-              ? today.Day.PrecipitaionType
-              : null,
-            precipitaionIntensity: today.Day.HasPrecipitation
-              ? today.Day.PrecipitaionIntensity
-              : null,
-          },
-          night: {
-            icon: 34,
-            iconPhrase: 'Predominantemente aberto',
-            hasPrecipitation: today.Night.HasPrecipitation,
-            precipitaionType: today.Night.HasPrecipitation
-              ? today.Night.PrecipitaionType
-              : null,
-            precipitaionIntensity: today.Night.HasPrecipitation
-              ? today.Night.PrecipitaionIntensity
-              : null,
-          },
-        },
+        dailyForecast,
+      })
+
+      // this.setState({
+      //   dailyForecast: [
+      //     this.dailyForecast(res.data.DailyForecasts[0]),
+      //     this.dailyForecast(res.data.DailyForecasts[1]),
+      //     this.dailyForecast(res.data.DailyForecasts[2]),
+      //     this.dailyForecast(res.data.DailyForecasts[3]),
+      //     this.dailyForecast(res.data.DailyForecasts[4]),
+      //   ],
+      // })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  get12HourForecast = async () => {
+    try {
+      const cityCode = this.state.cityCode
+      const res = await axios.get(
+        `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${cityCode}?apikey=m7YVXQ5NAYdGRbXEygi5hyuFUcT9657F&language=pt-br&metric=true`
+      )
+      const hourly12Forecast = []
+      for (hour of res.data) {
+        hourly12Forecast.push(this.hourlyForecast(hour))
+      }
+      this.setState({
+        hourly12Forecast,
       })
     } catch (e) {
       console.log(e)
     }
+  }
+
+  dailyForecast = nthDayForecast => {
+    const forecast = {
+      date: nthDayForecast.Date,
+      minTemp: Math.round(nthDayForecast.Temperature.Minimum.Value),
+      maxTemp: Math.round(nthDayForecast.Temperature.Maximum.Value),
+      day: {
+        icon: nthDayForecast.Day.Icon,
+        iconPhrase: nthDayForecast.Day.IconPhrase,
+        hasPrecipitation: nthDayForecast.Day.HasPrecipitation,
+        precipitaionType: nthDayForecast.Day.HasPrecipitation
+          ? nthDayForecast.Day.PrecipitaionType
+          : null,
+        precipitaionIntensity: nthDayForecast.Day.HasPrecipitation
+          ? nthDayForecast.Day.PrecipitaionIntensity
+          : null,
+      },
+      night: {
+        icon: nthDayForecast.Night.Icon,
+        iconPhrase: nthDayForecast.Night.IconPhrase,
+        hasPrecipitation: nthDayForecast.Night.HasPrecipitation,
+        precipitaionType: nthDayForecast.Night.HasPrecipitation
+          ? nthDayForecast.Night.PrecipitaionType
+          : null,
+        precipitaionIntensity: nthDayForecast.Night.HasPrecipitation
+          ? nthDayForecast.Night.PrecipitaionIntensity
+          : null,
+      },
+    }
+    return forecast
+  }
+
+  hourlyForecast = nthHourDay => {
+    const hourForecast = {
+      date: nthHourDay.DateTime,
+      icon: nthHourDay.WeatherIcon,
+      iconPhrase: nthHourDay.IconPhrase,
+      hasPrecipitation: nthHourDay.HasPrecipitation,
+      isDaylight: nthHourDay.IsDaylight,
+      temp: nthHourDay.Temperature.Value,
+      precipitaionProbability: nthHourDay.precipitaionProbability,
+    }
+    return hourForecast
   }
 
   render() {
@@ -210,46 +165,25 @@ export default class Home extends Component {
                 {this.state.current.iconPhrase}
               </Text>
               <Text style={[styles.headerMaxMin, styles.headerText]}>
-                Máx.: {this.state.todayForecast.maxTemp}° Mín.:{' '}
-                {this.state.todayForecast.minTemp}°
+                Máx.: {this.state.dailyForecast[0].maxTemp}° Mín.:{' '}
+                {this.state.dailyForecast[0].minTemp}°
               </Text>
             </View>
 
             <View style={styles.main}>
               <View style={[styles.infoContainer, styles.infoHourly]}>
                 <Text style={styles.infoContainerTitle}>Infos hora a hora</Text>
-                <ScrollView horizontal>
-                  <View style={{ flexDirection: 'row' }}>
-                    <HourlyWeather
-                      hour="Agora"
-                      temp="22"
+                <FlatList
+                  horizontal
+                  data={this.state.hourly12Forecast}
+                  keyExtractor={item => item.date}
+                  renderItem={({ item }) => (
+                    <HourlyForecast
+                      hour={item.date.substring(11, 13)}
+                      temp={item.temp}
                     />
-                    <HourlyWeather
-                      hour="12"
-                      temp="23"
-                    />
-                    <HourlyWeather
-                      hour="13"
-                      temp="24"
-                    />
-                    <HourlyWeather
-                      hour="14"
-                      temp="25"
-                    />
-                    <HourlyWeather
-                      hour="15"
-                      temp="23"
-                    />
-                    <HourlyWeather
-                      hour="16"
-                      temp="22"
-                    />
-                    <HourlyWeather
-                      hour="17"
-                      temp="21"
-                    />
-                  </View>
-                </ScrollView>
+                  )}
+                />
               </View>
 
               <View style={[styles.infoContainer, styles.infoDaily]}>
@@ -257,29 +191,29 @@ export default class Home extends Component {
                   Previsão para 5 dias
                 </Text>
                 <DailyForecast
-                  day={this.state.todayForecast.date.substring(8, 10)}
-                  minTemp={this.state.todayForecast.minTemp}
-                  maxTemp={this.state.todayForecast.maxTemp}
+                  day={this.state.dailyForecast[0].date.substring(8, 10)}
+                  minTemp={this.state.dailyForecast[0].minTemp}
+                  maxTemp={this.state.dailyForecast[0].maxTemp}
                 />
                 <DailyForecast
-                  day={this.state.day2Forecast.date.substring(8, 10)}
-                  minTemp={this.state.day2Forecast.minTemp}
-                  maxTemp={this.state.day2Forecast.maxTemp}
+                  day={this.state.dailyForecast[1].date.substring(8, 10)}
+                  minTemp={this.state.dailyForecast[1].minTemp}
+                  maxTemp={this.state.dailyForecast[1].maxTemp}
                 />
                 <DailyForecast
-                  day={this.state.day3Forecast.date.substring(8, 10)}
-                  minTemp={this.state.day3Forecast.minTemp}
-                  maxTemp={this.state.day3Forecast.maxTemp}
+                  day={this.state.dailyForecast[2].date.substring(8, 10)}
+                  minTemp={this.state.dailyForecast[2].minTemp}
+                  maxTemp={this.state.dailyForecast[2].maxTemp}
                 />
                 <DailyForecast
-                  day={this.state.day4Forecast.date.substring(8, 10)}
-                  minTemp={this.state.day4Forecast.minTemp}
-                  maxTemp={this.state.day4Forecast.maxTemp}
+                  day={this.state.dailyForecast[3].date.substring(8, 10)}
+                  minTemp={this.state.dailyForecast[3].minTemp}
+                  maxTemp={this.state.dailyForecast[3].maxTemp}
                 />
                 <DailyForecast
-                  day={this.state.day5Forecast.date.substring(8, 10)}
-                  minTemp={this.state.day5Forecast.minTemp}
-                  maxTemp={this.state.day5Forecast.maxTemp}
+                  day={this.state.dailyForecast[4].date.substring(8, 10)}
+                  minTemp={this.state.dailyForecast[4].minTemp}
+                  maxTemp={this.state.dailyForecast[4].maxTemp}
                 />
               </View>
               <TouchableOpacity onPress={this.get5DayForecast}>
@@ -287,6 +221,9 @@ export default class Home extends Component {
               </TouchableOpacity>
               <TouchableOpacity onPress={this.getCurrentWeather}>
                 <Text>Get today weather</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={this.get12HourForecast}>
+                <Text>Get 12 hour forecast</Text>
               </TouchableOpacity>
             </View>
           </ScrollView>
